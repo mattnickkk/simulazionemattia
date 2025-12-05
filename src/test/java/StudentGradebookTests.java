c4055cace562970eb675a33da2e28aff42651b33
mattnickkk <mattnickgaming00@gmail.com>
2025-12-05 12:56:41 +0100

    Creato Test nomeVuotoLanciaEccezione

---
+++ src/test/java/StudentGradebookTests.java
@@ -0,0 +1,31 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+
+import javax.swing.*;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class StudentGradebookTests {
+    StudentGradebook gradebook;
+
+    @BeforeEach
+    public void setup() {
+        String nome = "Mattia";
+        gradebook = new StudentGradebook(nome);
+    }
+
+
+    @Test
+    public void nomeVuotoanciaEccezioneTest() {
+        String nome = "";
+        assertThrows(IllegalArgumentException.class, () -> {StudentGradebook gradebook = new StudentGradebook (nome);});
+    }
+
+
+
+
+
+
+}
